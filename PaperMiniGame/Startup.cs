2b7e22310ea75f0f1ac86c025a517a504849bfd8
Mekon18 <maksim.grummo@mail.ru>
2019-07-11 10:29:09 +0300

    init

---
+++ PaperMiniGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PaperMiniGame.Startup))]
+namespace PaperMiniGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
